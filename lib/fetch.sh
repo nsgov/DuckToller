46dd640dd57a68d3b387cbe4f616b6c6dd55e94f
David Nordlund <dave@davesmbp.local>
2013-03-01 23:08:48 -0400

    Favour curl.

--- lib/fetch.sh
+++ lib/fetch.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 cd $1 || exit 1
-WGET="`which wget`"
 CURL="`which curl`"
-if [ -n "$WGET" ]; then
-	exec "$WGET" "$2"
-elif [ -n "$CURL" ]; then
+WGET="`which wget`"
+if [ -n "$CURL" ]; then
 	exec "$CURL" -\#LO "$2"
+elif [ -n "$WGET" ]; then
+	exec "$WGET" "$2"
 else
-	echo "Neither wget or curl are available"
+	echo "Neither curl or wget are available"
 	exit 1;
 fi
