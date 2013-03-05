a41e0d7e30757d94b107417d26a17c510598f4f6
David Nordlund <github@tangledweb.info>
2013-03-05 17:49:29 -0400

    Add params to data-twitterfeed attribute, to support "max" parameter.

--- twitter/tweetcache.js
+++ twitter/tweetcache.js
@@ -3,13 +3,19 @@ var TweetCache = {
 	run: function() {
 		var tags = document.querySelectorAll("*[data-twitterfeed]");
 		for (var i = tags.length, tag, m; (i--) && (tag = tags[i]);) {
-			if ((m = tag.getAttribute("data-twitterfeed").match(/^([@#])(\w+)$/))) {
-				var url = this.URL + m[0];
+			var tf = tag.getAttribute("data-twitterfeed").split(';');
+			var params = {};
+			for (var j = tf.length; --j;) {
+				var p = tf[j].match("\s*(\w+)\s*=\s(\S+)\s*");
+				if (p) params[p[1]] = p[2];
+			}
+			if ((m = tf[0].match(/^([@#])(\w+)$/))) {
+				var url = this.URL + escape(m[0]);
 				if (!(url in this.feeders)) {
 					this.feeders[url] = new TweetCache.Feeder(url);
 					this.remaining++;
 				}
-				this.feeders[url].feedTag(tag)
+				this.feeders[url].feedTag(tag, params)
 			}
 		}
 		for (var f in this.feeders)
@@ -24,19 +30,24 @@ var TweetCache = {
 	Feeder: function(url) {		// For each feed URL, fetch tweets & put into document
 		this.url = url;
 		this.tags = [];
+		this.max = 0;
 	},
 	feeders: [],
 	remaining: 0,
 	canRun: document.querySelectorAll && window.XMLHttpRequest
 };
 TweetCache.Feeder.prototype = {
-	feedTag: function(tag) {
-		this.tags.push(tag);
+	feedTag: function(tag, params) {
+		if (!params.max) params.max = 0;
+		this.max = Math.max(this.max, params.max-0);
+		this.tags.push({tag: tag, params: params});
 		tag.setAttribute("aria-busy", "true");
 	},
 	load: function() {
-		var it = this, cors = this.cors = new TweetCache.Loader();
-		cors.open("GET", this.url);
+		var it = this, cors = this.cors = new TweetCache.Loader(), qs = '';
+		if (this.max)
+			qs = '?max=' + max;
+		cors.open("GET", this.url+qs);
 		cors.onerror   = function() { it.failed(cors.statusText||"Loading failed. :("); };
 		cors.ontimeout = function() { it.failed("Connection timeout."); };
 		cors.onload    = function() { it.loaded(cors.responseXML); };
@@ -44,7 +55,15 @@ TweetCache.Feeder.prototype = {
 	},
 	loaded: function(atom) {
 		var xmlns = 'http://www.w3.org/2005/Atom';
-		var entries = atom.getElementsByTagNameNS(xmlns, 'entry');
+		var entrylist = atom.getElementsByTagNameNS(xmlns, 'entry'), entries = [];
+		var total = entrylist.length;
+		if (this.max)
+			total = Math.min(total, this.max);
+		for (var i = 0; i < total; entries[i] = entrylist[i]);
+		entrylist = null;
+		for (var i=this.tags.length, t; i && (t=this.tags[--i]); ) {
+			this.tags[i].tag.innerHTML = html;
+		}
 		var max = entries.length;
 		var html = '';
 		for (var i=0; i < max; i++) {
