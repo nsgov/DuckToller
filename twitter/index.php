01a364b3213e60e3dfb6bfde8b6bc0c47a1e1d09
David Nordlund <github@tangledweb.info>
2013-03-21 13:44:59 -0300

    Require only the needed php files.

--- twitter/index.php
+++ twitter/index.php
@@ -1,16 +1,17 @@
 <?php
 
 require_once('../base/ducktoller.phps');
-require_once('./twitterfeed.phps');
-require_once('./twitteravatar.phps');
 
 $ducktoller = new DuckToller("../ducktoller.ini");
 $duck = null;
 
-if (isset($_GET['feed']))
+if (isset($_GET['feed'])) {
+	require_once('./twitterfeed.phps');
 	$duck = new TwitterFeed($ducktoller, $_GET['feed']);
-elseif (isset($_GET['avatar']))
+} elseif (isset($_GET['avatar'])) {
+	require_once('./twitteravatar.phps');
 	$duck = new TwitterAvatar($ducktoller, $_GET['avatar']);
+}
 
 if ($duck)
 	$ducktoller->retrieve($duck);
