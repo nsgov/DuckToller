ce6369d21de92b51203aa405f3f1823ce3df6ce3
David Nordlund <github@tangledweb.info>
2013-03-21 08:41:06 -0300

    Rename tweetcache.phps to twitterfeed.phps.

--- twitter/index.php
+++ twitter/index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once('../base/ducktoller.phps');
-require_once('./tweetcache.phps');
+require_once('./twitterfeed.phps');
 require_once('./twitteravatar.phps');
 
 $ducktoller = new DuckToller("../ducktoller.ini");
@@ -14,7 +14,7 @@ if (isset($_GET['feed'])) {
 		if (!$key)
 			throw new Exception("Don't forget your keys! $name needs to be set in $keysfile.");
 
-	$duck = new TweetCache($ducktoller, $keys, $_GET['feed']);
+	$duck = new TwitterFeed($ducktoller, $keys, $_GET['feed']);
 } elseif (isset($_GET['avatar'])) {
 	$duck = new TwitterAvatar($ducktoller, $_GET['avatar']);
 }
